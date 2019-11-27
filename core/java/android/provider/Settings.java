d8cf6cc751c6b7681195c5b4f73781fa3fcd937e
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: bring back old style mobile data indicators
    TODO maybe add settings observer to do update faster then
    waiting for the next update
    
    Change-Id: I3367bf0b7ff5b6483efc820a86dec8469af6e034
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    -----
    
    SystemUI: Update mobile data icon instantly when toggled
    
    Change-Id: Ic3cb0749b778d9caf2493757ac3580a743e53c19
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    -----
    
    SystemUI: status bar mobile indicator - code sharing is caring
    
    and helps to not create stupid bugs as the last one
    
    Change-Id: I19343ab6083d5d24b35ac0489cbfb17f16006b82
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5623,6 +5623,12 @@ public final class Settings {
         public static final String VOLUME_MEDIA_OUTPUT_TOGGLE = "volume_media_output_toggle";
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
